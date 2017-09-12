8fff54a660aecf8d30fdfc2a270269d96b761241
Josh Abraham <sinisterpatrician@gmail.com>
2017-09-12 10:39:55 -0400

    Remove garbage files; add some properties

--- overlord.py
+++ overlord.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
 
-#from mining import Zerg
 from .zerg import Zerg
+
 
 class Overlord(Zerg):
     def __init__(self, ticks, refined_minerals):
         super().__init__()
         self.ticks = ticks
         self.refined_minerals = refined_minerals
+        self.maps = dict()
+        self.zerg = dict() # TODO: @property this
 
     def action(self):
         pass
