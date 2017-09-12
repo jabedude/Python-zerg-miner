4922c2072d3e01b142ff873a13f7d864cfb4e394
Josh Abraham <sinisterpatrician@gmail.com>
2017-09-12 15:35:31 -0400

    Add steps() method and tests for drone

--- drone.py
+++ drone.py
@@ -9,9 +9,19 @@ class Drone(Zerg):
         self.health = health
         self.capacity = capacity
         self.moves = moves
+        self.step_count = 0
 
     def action(self, context):
         '''
         Entry point of action for Overlord. Context not currently used.
         '''
+        self.step_count += 1
         pass
+
+    def steps(self):
+        '''Return the number of steps taken by drone'''
+        return self.step_count
+
+    @classmethod
+    def get_init_cost():
+        return 0
--- tests/test_drone.py
+++ tests/test_drone.py
@@ -17,3 +17,16 @@ class TestDrone(unittest.TestCase):
         self.assertEqual(d.health, 40)
         self.assertEqual(d.capacity, 10)
         self.assertEqual(d.moves, 1)
+
+    def test_drone_non_default_attributes(self):
+        d = Drone(1, 2, 3)
+        self.assertEqual(d.health, 1)
+        self.assertEqual(d.capacity, 2)
+        self.assertEqual(d.moves, 3)
+
+    def test_drone_steps_count(self):
+        d = Drone()
+        for _ in range(5):
+            d.action(None)
+        
+        self.assertEqual(d.steps(), 5)
