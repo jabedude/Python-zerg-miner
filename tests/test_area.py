24c35e9bdd7d1b8dd943509f5367effe3ad86075
Josh Abraham <sinisterpatrician@gmail.com>
2017-09-13 12:27:46 -0400

    Add unit tests for Area class

---
+++ tests/test_area.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import unittest
+import sys
+sys.path.append("..")
+from area import Area
+
+
+class TestScout(unittest.TestCase):
+
+    def test_area_creation(self):
+        a = Area()
+        self.assertIsInstance(a, Area)
+
+    def test_area_set_coordinate(self):
+        a = Area()
+        coordinates = (1, 0)
+        tile = " "
+        a[coordinates] = tile
+        self.assertEqual(a[coordinates], tile)
+
+    def test_area_set_multiple_coordinate(self):
+        a = Area()
+        coord = (1, 0)
+        tile = "*"
+        a[coord] = tile
+        coord2 = (1, 1)
+        a[coord2] = tile
+        self.assertEqual(a[coord], tile)
+        self.assertEqual(a[coord2], tile)
