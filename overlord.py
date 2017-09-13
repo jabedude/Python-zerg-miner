f3bafec4c7169707ed20c7edc6e5819e47d458f1
Josh Abraham <sinisterpatrician@gmail.com>
2017-09-13 09:02:30 -0400

    Change maps to be a class attribute in overlord

--- overlord.py
+++ overlord.py
@@ -1,15 +1,23 @@
 #!/usr/bin/env python3
 
 from mining.zerg import Zerg
+from mining.drone import Drone
 
 
 class Overlord(Zerg):
+
+    maps = dict()
+
     def __init__(self, ticks, refined_minerals):
         super().__init__()
         self.ticks = ticks
         self.refined_minerals = refined_minerals
-        self.maps = dict()
-        self.zerg = dict() # TODO: @property this
+        self.zerg = dict()  # TODO: @property this
+        ### TEMP ###
+        for _ in range(3):
+            z = Drone()
+            self.zerg[id(z)] = z
+        ### TEMP ###
 
     def add_map(self, map_id, summary):
         '''Adds an identifier for a map and a summary of the map'''
@@ -19,8 +27,16 @@ class Overlord(Zerg):
         '''
         Entry point of action for Overlord. Context not currently used.
         '''
-        pass
+        ### TEMP ###
+        import random
+        act = random.randint(0, 3)
+        if act == 0:
+            return "RETURN {}".format(random.choice(list(self.zerg.keys())))
+        else:
+            return "DEPLOY {} {}".format(random.choice(list(self.zerg.keys())),
+                    random.choice(list(self.maps.keys())))
+        ### TEMP ###
 
-    def _generate_drones(self):
+    def _generate_drones(self, refined_minerals):
         '''Calculate quantities of drones to create'''
         raise NotImplementedError
