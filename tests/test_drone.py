b7b59afd5311b8c4ae23874680342c9bf39f69e8
Josh Abraham <sinisterpatrician@gmail.com>
2017-09-14 09:35:54 -0400

    Remove redundant test from test_drone

--- tests/test_drone.py
+++ tests/test_drone.py
@@ -19,14 +19,6 @@ class TestDrone(unittest.TestCase):
         self.assertEqual(d.capacity, 10)
         self.assertEqual(d.moves, 1)
 
-    def test_drone_steps_count(self):
-        mock_Context = mock.Mock()
-        mock_Context.north = mock_Context.south = mock_Context.east = mock_Context.west = '*'
-        d = Drone()
-        for _ in range(5):
-            d.action(mock_Context)  # TODO: might need to change this
-        self.assertEqual(d.steps(), 5)
-
     def test_drone_get_init_cost(self):
         cost = Drone().get_init_cost()
         self.assertEqual(cost, 9)
