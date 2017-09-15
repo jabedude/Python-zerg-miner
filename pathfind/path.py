c19e318d31ca4f95845cc2c1848aa32fbc6e8702
Josh Abraham <sinisterpatrician@gmail.com>
2017-09-15 11:32:49 -0400

    Add Bredth-first-search to graph

--- pathfind/path.py
+++ pathfind/path.py
@@ -13,3 +13,38 @@ def recursive_path(goal, grid):
 
 def linear_distance(point_one, point_two):
     return hypot(point_one[0] - point_two[0], point_one[1] - point_two[1])
+
+def area_to_graph(area):
+    graph = dict()
+    print()
+
+    for coordinate in area:
+        graph[coordinate] = []
+        x = coordinate[0]
+        y = coordinate[1]
+        north = (x, y + 1)
+        south = (x, y - 1)
+        east = (x + 1, y)
+        west = (x - 1, y)
+        if area[north] in ' *':
+            graph[coordinate].append(north)
+        if area[south] in ' *':
+            graph[coordinate].append(south)
+        if area[east] in ' *':
+            graph[coordinate].append(east)
+        if area[west] in ' *':
+            graph[coordinate].append(west)
+
+    print(graph)
+    return graph
+
+def bfs_paths(graph, start, goal):
+    '''http://eddmann.com/posts/depth-first-search-and-breadth-first-search-in-python/'''
+    queue = [(start, [start])]
+    while queue:
+        (vertex, path) = queue.pop(0)
+        for next in set(graph[vertex]) - set(path):
+            if next == goal:
+                yield path + [next]
+            else:
+                queue.append((next, path + [next]))
--- tests/test_path.py
+++ tests/test_path.py
@@ -4,6 +4,7 @@ import unittest
 import sys
 sys.path.append("..")
 from pathfind import path
+from area import Area
 
 
 class TestPath(unittest.TestCase):
@@ -13,3 +14,22 @@ class TestPath(unittest.TestCase):
         two = (1, 5)
         distance = path.linear_distance(one, two)
         self.assertEqual(distance, 5)
+
+    def test_area_to_graph(self):
+        a = Area()
+        test_map = {
+            (0, 0): '_',
+            (0, 1): ' ',
+            (1, 0): '#',
+            (1, -1): '#',
+            (0, -1): '#',
+            (-1, 0): ' ',
+            (-1, -2): ' ',
+            (-1, 1): ' ',
+            (1, 1): ' ',
+            (0, 2): '*'
+        }
+        a._data = test_map
+        g = path.area_to_graph(a)
+        p = path.bfs_paths(g, (0, 0), (0, 2))
+        print(list(p))
