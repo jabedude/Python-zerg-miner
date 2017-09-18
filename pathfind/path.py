1d4182bdb140ee9bb1eb77b050326c71d0b88081
Josh Abraham <sinisterpatrician@gmail.com>
2017-09-18 15:00:45 -0400

    Add pathfinding helper methods

--- pathfind/path.py
+++ pathfind/path.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 from math import hypot
+import copy
 
 
 class Path:
@@ -14,9 +15,11 @@ def recursive_path(goal, grid):
 def linear_distance(point_one, point_two):
     return hypot(point_one[0] - point_two[0], point_one[1] - point_two[1])
 
-def area_to_graph(area):
+def area_to_graph(area, goal=None):
     graph = dict()
-    print()
+    area = copy.deepcopy(area)
+    if goal is not None:
+        area[goal] = ' '
 
     for coordinate in area:
         graph[coordinate] = []
@@ -26,16 +29,14 @@ def area_to_graph(area):
         south = (x, y - 1)
         east = (x + 1, y)
         west = (x - 1, y)
-        if area[north] in ' *':
+        if area[north] in ' _':
             graph[coordinate].append(north)
-        if area[south] in ' *':
+        if area[south] in ' _':
             graph[coordinate].append(south)
-        if area[east] in ' *':
+        if area[east] in ' _':
             graph[coordinate].append(east)
-        if area[west] in ' *':
+        if area[west] in ' _':
             graph[coordinate].append(west)
-
-    print(graph)
     return graph
 
 def bfs_paths(graph, start, goal):
@@ -48,3 +49,22 @@ def bfs_paths(graph, start, goal):
                 yield path + [next]
             else:
                 queue.append((next, path + [next]))
+
+def shortest_path(graph, start, goal):
+    try:
+        return next(bfs_paths(graph, start, goal))
+    except StopIteration:
+        return None
+
+def generate_cardinality(coordinates):
+    directions = list()
+    for coord, prev in zip(coordinates[1:], coordinates):
+        if (prev[0], prev[1] + 1) == coord:
+            directions.append('NORTH')
+        elif (prev[0], prev[1] - 1) == coord:
+            directions.append('SOUTH')
+        elif (prev[0] + 1, prev[1]) == coord:
+            directions.append('EAST')
+        elif (prev[0] - 1, prev[1]) == coord:
+            directions.append('WEST')
+    return directions
